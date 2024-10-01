bb8f707fdb9694deb2146d348ab90e8c0cb7b099
netstermonir <monirhossain6163@gmail.com>
2024-10-01 11:22:50 +0600

    authjs

--- auth.ts
+++ auth.ts
@@ -2,13 +2,13 @@ import NextAuth from "next-auth";
 import prisma from "./lib/prisma";
 
 import { PrismaAdapter } from "@auth/prisma-adapter";
+import { Adapter } from "next-auth/adapters";
 import authConfig from "./auth.config";
 
-
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    adapter: PrismaAdapter(prisma),
-    session: {
-        strategy: "jwt",
-    },
-    ...authConfig
-})
+  adapter: PrismaAdapter(prisma) as Adapter,
+  session: {
+    strategy: "jwt",
+  },
+  ...authConfig,
+});
